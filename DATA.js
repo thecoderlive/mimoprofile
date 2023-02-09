9c28dfa3bf6dc843fb5904faa5896e8c20279c6d
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-09 13:39:34 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,36 @@
+export const item = {
+    "id": "71baf92d-f953-4df2-b296-3f08fa4733fc",
+    "setting_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/340.png",
+    "email": "Water action visit can. City new prevent lay here keep gun. Cut customer say religious close.",
+    "build_app": "Positive scene energy close admit third identify. Receive myself decide show.",
+    "progress_percent": 2482,
+    "achievement": "employee",
+    "achievement_count": 7098,
+    "achievement_list": [
+        {
+            "id": "23f0035d-50a0-4023-9a2f-515d6488b197",
+            "achievement_logo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/167.png",
+            "achievement_title": "use"
+        },
+        {
+            "id": "98481c3b-56dd-4097-861a-9998090e9108",
+            "achievement_logo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/95.png",
+            "achievement_title": "action"
+        },
+        {
+            "id": "7777b677-06e6-4801-b8b5-71e4ae6468f3",
+            "achievement_logo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/14.png",
+            "achievement_title": "health"
+        },
+        {
+            "id": "a411ce18-e38f-4c34-852c-88b202d2ebce",
+            "achievement_logo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/58.png",
+            "achievement_title": "nation"
+        },
+        {
+            "id": "06810f31-6bc6-4f55-83e8-f5ac37f8ef31",
+            "achievement_logo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/210.png",
+            "achievement_title": "movie"
+        }
+    ]
+}
